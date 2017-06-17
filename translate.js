c4f2e36fc94fe31204be8873cadbd8268356426e
Alice Pote <alice.writes.wrongs@gmail.com>
2017-06-17 10:01:15 -0400

    make translate.js take a command-line argument

--- translate.js
+++ translate.js
@@ -3,25 +3,32 @@ const translate = require('google-translate-api')
 const { stringify } = require('csv')
 const chalk = require('chalk')
 
-let words = JSON.parse(String(fs.readFileSync('words.json')))
+const csvFilename = jsonFilename => (
+  `${jsonFilename.replace(/\..+$/, "")}.csv`
+);
 
-let translatedWords = []
+if (process.argv.length < 3) {
+  console.log("no arguments provided, usage is:\n");
+  console.log("\tnode translate.js word_file.json\n");
+} else {
+  let words = JSON.parse(String(fs.readFileSync(process.argv[2])));
 
-Promise.all(words.map(word => {
-  return translate(word, { to: 'en' })
-    .then(res => {
-      console.log(`translating ${chalk.magenta(word)} to ${chalk.blue(res.text)}`);
-      return [word, res.text]
+  Promise.all(words.map(word => {
+    return translate(word, { to: 'en' })
+      .then(res => {
+        console.log(`translating ${chalk.magenta(word)} to ${chalk.blue(res.text)}`);
+        return [word, res.text]
+      })
+      .catch(console.error);
+  })).then(translated => {
+    console.log('done translating ✔\n');
+
+    let newFilename = csvFilename(process.argv[2]);
+    console.log(`writing translations to ${newFilename}...`);
+
+    stringify(translated, (err, output) => {
+      fs.writeFileSync(newFilename, output);
+      console.log(chalk.green("done!"));
     })
-    .catch(console.error);
-})).then(translated => {
-  console.log('done translating ✔\n');
-  console.log('writing translations to disk...');
-
-  fs.writeFileSync("translated_words.json", JSON.stringify(translated));
-
-  stringify(translated, (err, output) => {
-    fs.writeFileSync("translated_words.csv", output);
-    console.log(chalk.green("done!"));
-  })
-});
+  });
+}
