3b9430798e89b6269cf866b80b1cb11d91dd203b
Alice Pote <alice.writes.wrongs@gmail.com>
2017-06-17 09:13:39 -0400

    I made the thing work! nice.

--- translate.js
+++ translate.js
@@ -1,17 +1,26 @@
 const fs = require('fs')
-
 const translate = require('google-translate-api')
+const { stringify } = require('csv')
 
 let words = JSON.parse(String(fs.readFileSync('words.json')))
 
 let translatedWords = []
 
 Promise.all(words.map(word => {
-  console.log(`translating ${word}`);
   return translate(word, { to: 'en' })
-    .then(res => [word, res.text])
+    .then(res => {
+      console.log(`translating ${word} to ${res.text}`);
+      return [word, res.text]
+    })
     .catch(console.error);
 })).then(translated => {
-  console.log('done translating');
+  console.log('done translating\n');
+  console.log('writing translations to disk...');
+
   fs.writeFileSync("translated_words.json", JSON.stringify(translated));
+
+  stringify(translated, (err, output) => {
+    fs.writeFileSync("translated_words.csv", output);
+    console.log("done!");
+  })
 });
