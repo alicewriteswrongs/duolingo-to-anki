fceb78f9d0c430f58fef38311b0e94867b48a804
Alice Pote <alice.writes.wrongs@gmail.com>
2017-06-17 10:51:40 -0400

    rewrite it to use commander

--- translate.js
+++ translate.js
@@ -2,21 +2,38 @@ const fs = require('fs')
 const translate = require('google-translate-api')
 const { stringify } = require('csv')
 const chalk = require('chalk')
+const program = require('commander')
 
 const csvFilename = jsonFilename => (
   `${jsonFilename.replace(/\..+$/, '')}.csv`
 )
 
-if (process.argv.length < 3) {
-  console.log('no arguments provided, usage is:\n')
-  console.log('\tnode translate.js word_file.json\n')
+program
+  .description('translate your duolingo words and output a CSV for anki!')
+  .option('-t, --to <code>', 'target language (defaults to English)')
+  .option('-f, --from <code>', 'source language (defaults to guessing)')
+  .option('-j, --json <path>', 'path to JSON file containing words to translate')
+  .parse(process.argv);
+
+if (! program.json ) {
+  program.help();
 } else {
-  let words = JSON.parse(String(fs.readFileSync(process.argv[2])))
+  let words = JSON.parse(String(fs.readFileSync(program.json)));
+
+  let translateOptions = { to: 'en' }
+
+  if ( program.to ) {
+    translateOptions.to = program.to
+  }
+
+  if ( program.from ) {
+    translateOptions.from = program.from
+  }
 
   console.log('translating...');
 
   Promise.all(words.map(word => {
-    return translate(word, { to: 'en', from: 'fr' })
+    return translate(word, translateOptions)
       .then(res => {
         console.log(`translating ${chalk.magenta(word)} to ${chalk.blue(res.text)}`)
         return [word, res.text]
@@ -25,7 +42,7 @@ if (process.argv.length < 3) {
   })).then(translated => {
     console.log('done translating ✔\n')
 
-    let newFilename = csvFilename(process.argv[2])
+    let newFilename = csvFilename(program.json);
     console.log(`writing translations to ${newFilename}...`)
 
     stringify(translated, (err, output) => { // eslint-disable-line
